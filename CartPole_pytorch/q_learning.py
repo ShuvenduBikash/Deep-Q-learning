6ed6bf7c683ad30859590a9570e62c795567c7a6
Shuvendu Bikash <bikash11roy@gmail.com>
2017-11-23 12:55:29 +0600

    defining ReplayMemory

--- CartPole_pytorch/q_learning.py
+++ CartPole_pytorch/q_learning.py
@@ -15,3 +15,48 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import torchvision.transforms as T
 
+env = gym.make('CartPole-v0').unwrapped
+plt.ion()
+
+# if gpu is used
+use_cuda = torch.cuda.is_available()
+FloatTensor = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
+LongTensor = torch.cuda.LongTensor if use_cuda else torch.LongTensor
+ByteTensor = torch.cuda.ByteTensor if use_cuda else torch.ByteTensor
+Tensor = FloatTensor
+
+'''
+# Replay Memory
+We’ll be using experience replay memory for training our DQN. It stores the transitions that the agent observes,
+allowing us to reuse this data later. By sampling from it randomly, the transitions that build up a batch are decorrelated.
+  It has been shown that this greatly stabilizes and improves the DQN training procedure.
+
+For this, we’re going to need two classses:
+
+    Transition - a named tuple representing a single transition in our environment
+    ReplayMemory - a cyclic buffer of bounded size that holds the transitions observed recently. It also implements a
+                   .sample() method for selecting a random batch of transitions for training.
+'''
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        # saves a transition
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
