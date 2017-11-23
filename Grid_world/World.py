e0d0d9e38799c332975fbebe8f0d8d30851ab162
Shuvendu Bikash <bikash11roy@gmail.com>
2017-11-23 14:39:58 +0600

    converted into python 3

--- Grid_world/World.py
+++ Grid_world/World.py
@@ -1,5 +1,6 @@
 __author__ = 'philippe'
-from Tkinter import *
+from tkinter import *
+
 master = Tk()
 
 triangle_size = 0.1
@@ -9,8 +10,8 @@ Width = 100
 (x, y) = (5, 5)
 actions = ["up", "down", "left", "right"]
 
-board = Canvas(master, width=x*Width, height=y*Width)
-player = (0, y-1)
+board = Canvas(master, width=x * Width, height=y * Width)
+player = (0, y - 1)
 score = 1
 restart = False
 walk_reward = -0.04
@@ -22,24 +23,24 @@ cell_scores = {}
 
 def create_triangle(i, j, action):
     if action == actions[0]:
-        return board.create_polygon((i+0.5-triangle_size)*Width, (j+triangle_size)*Width,
-                                    (i+0.5+triangle_size)*Width, (j+triangle_size)*Width,
-                                    (i+0.5)*Width, j*Width,
+        return board.create_polygon((i + 0.5 - triangle_size) * Width, (j + triangle_size) * Width,
+                                    (i + 0.5 + triangle_size) * Width, (j + triangle_size) * Width,
+                                    (i + 0.5) * Width, j * Width,
                                     fill="white", width=1)
     elif action == actions[1]:
-        return board.create_polygon((i+0.5-triangle_size)*Width, (j+1-triangle_size)*Width,
-                                    (i+0.5+triangle_size)*Width, (j+1-triangle_size)*Width,
-                                    (i+0.5)*Width, (j+1)*Width,
+        return board.create_polygon((i + 0.5 - triangle_size) * Width, (j + 1 - triangle_size) * Width,
+                                    (i + 0.5 + triangle_size) * Width, (j + 1 - triangle_size) * Width,
+                                    (i + 0.5) * Width, (j + 1) * Width,
                                     fill="white", width=1)
     elif action == actions[2]:
-        return board.create_polygon((i+triangle_size)*Width, (j+0.5-triangle_size)*Width,
-                                    (i+triangle_size)*Width, (j+0.5+triangle_size)*Width,
-                                    i*Width, (j+0.5)*Width,
+        return board.create_polygon((i + triangle_size) * Width, (j + 0.5 - triangle_size) * Width,
+                                    (i + triangle_size) * Width, (j + 0.5 + triangle_size) * Width,
+                                    i * Width, (j + 0.5) * Width,
                                     fill="white", width=1)
     elif action == actions[3]:
-        return board.create_polygon((i+1-triangle_size)*Width, (j+0.5-triangle_size)*Width,
-                                    (i+1-triangle_size)*Width, (j+0.5+triangle_size)*Width,
-                                    (i+1)*Width, (j+0.5)*Width,
+        return board.create_polygon((i + 1 - triangle_size) * Width, (j + 0.5 - triangle_size) * Width,
+                                    (i + 1 - triangle_size) * Width, (j + 0.5 + triangle_size) * Width,
+                                    (i + 1) * Width, (j + 0.5) * Width,
                                     fill="white", width=1)
 
 
@@ -47,15 +48,16 @@ def render_grid():
     global specials, walls, Width, x, y, player
     for i in range(x):
         for j in range(y):
-            board.create_rectangle(i*Width, j*Width, (i+1)*Width, (j+1)*Width, fill="white", width=1)
+            board.create_rectangle(i * Width, j * Width, (i + 1) * Width, (j + 1) * Width, fill="white", width=1)
             temp = {}
             for action in actions:
                 temp[action] = create_triangle(i, j, action)
-            cell_scores[(i,j)] = temp
+            cell_scores[(i, j)] = temp
     for (i, j, c, w) in specials:
-        board.create_rectangle(i*Width, j*Width, (i+1)*Width, (j+1)*Width, fill=c, width=1)
+        board.create_rectangle(i * Width, j * Width, (i + 1) * Width, (j + 1) * Width, fill=c, width=1)
     for (i, j) in walls:
-        board.create_rectangle(i*Width, j*Width, (i+1)*Width, (j+1)*Width, fill="black", width=1)
+        board.create_rectangle(i * Width, j * Width, (i + 1) * Width, (j + 1) * Width, fill="black", width=1)
+
 
 render_grid()
 
@@ -65,7 +67,7 @@ def set_cell_score(state, action, val):
     triangle = cell_scores[state][action]
     green_dec = int(min(255, max(0, (val - cell_score_min) * 255.0 / (cell_score_max - cell_score_min))))
     green = hex(green_dec)[2:]
-    red = hex(255-green_dec)[2:]
+    red = hex(255 - green_dec)[2:]
     if len(red) == 1:
         red += "0"
     if len(green) == 1:
@@ -76,25 +78,26 @@ def set_cell_score(state, action, val):
 
 def try_move(dx, dy):
     global player, x, y, score, walk_reward, me, restart
-    if restart == True:
+    if restart:
         restart_game()
     new_x = player[0] + dx
     new_y = player[1] + dy
     score += walk_reward
     if (new_x >= 0) and (new_x < x) and (new_y >= 0) and (new_y < y) and not ((new_x, new_y) in walls):
-        board.coords(me, new_x*Width+Width*2/10, new_y*Width+Width*2/10, new_x*Width+Width*8/10, new_y*Width+Width*8/10)
+        board.coords(me, new_x * Width + Width * 2 / 10, new_y * Width + Width * 2 / 10, new_x * Width + Width * 8 / 10,
+                     new_y * Width + Width * 8 / 10)
         player = (new_x, new_y)
     for (i, j, c, w) in specials:
         if new_x == i and new_y == j:
             score -= walk_reward
             score += w
             if score > 0:
-                print "Success! score: ", score
+                print("Success! score: ", score)
             else:
-                print "Fail! score: ", score
+                print("Fail! score: ", score)
             restart = True
             return
-    #print "score: ", score
+            # print "score: ", score
 
 
 def call_up(event):
@@ -115,21 +118,25 @@ def call_right(event):
 
 def restart_game():
     global player, score, me, restart
-    player = (0, y-1)
+    player = (0, y - 1)
     score = 1
     restart = False
-    board.coords(me, player[0]*Width+Width*2/10, player[1]*Width+Width*2/10, player[0]*Width+Width*8/10, player[1]*Width+Width*8/10)
+    board.coords(me, player[0] * Width + Width * 2 / 10, player[1] * Width + Width * 2 / 10,
+                 player[0] * Width + Width * 8 / 10, player[1] * Width + Width * 8 / 10)
+
 
 def has_restarted():
     return restart
+
 
 master.bind("<Up>", call_up)
 master.bind("<Down>", call_down)
 master.bind("<Right>", call_right)
 master.bind("<Left>", call_left)
 
-me = board.create_rectangle(player[0]*Width+Width*2/10, player[1]*Width+Width*2/10,
-                            player[0]*Width+Width*8/10, player[1]*Width+Width*8/10, fill="orange", width=1, tag="me")
+me = board.create_rectangle(player[0] * Width + Width * 2 / 10, player[1] * Width + Width * 2 / 10,
+                            player[0] * Width + Width * 8 / 10, player[1] * Width + Width * 8 / 10, fill="orange",
+                            width=1, tag="me")
 
 board.grid(row=0, column=0)
 
