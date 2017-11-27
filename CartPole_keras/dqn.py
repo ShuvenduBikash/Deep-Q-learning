423dfb1a0663be40cb31d9f73fa115e84fca5bf7
Shuvendu Bikash <bikash11roy@gmail.com>
2017-11-27 12:56:00 +0600

    training the model

--- CartPole_keras/dqn.py
+++ CartPole_keras/dqn.py
@@ -49,9 +49,9 @@ class DQN:
 
             target_f = self.model.predict(state)
             target_f[0][action] = target
-            self.model.fit(state, target_f, epochs=1, verbose=1)
+            self.model.fit(state, target_f, epochs=1, verbose=0)
 
-        if self.epsilon>self.epsilon_min:
+        if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
     def laod(self, name):
@@ -64,3 +64,29 @@ class DQN:
 if __name__ == '__main__':
     env = gym.make('CartPole-v1')
     state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+
+    agent = DQN(state_size, action_size)
+    # agent.load("./save/cartpole-dqn.h5")
+    done = False
+    batch_size = 32
+
+    for e in range(EPISODES):
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+        for time in range(500):
+            # env.render()
+            action = agent.act(state)
+            next_state, reward, done, _ = env.step(action)
+            reward = reward if not done else -10
+            next_state = np.reshape(next_state, [1, state_size])
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                print("episode: {}/{}, score: {}, e: {:.2}"
+                      .format(e, EPISODES, time, agent.epsilon))
+                break
+        if len(agent.memory) > batch_size:
+            agent.replay(batch_size)
+            # if e % 10 == 0:
+            #     agent.save("./save/cartpole-dqn.h5")
